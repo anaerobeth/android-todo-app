cccbafdb48ada264895d85f5327dec7ef8a7710f
Beth Tenorio <betsky98@gmail.com>
2017-03-02 08:39:49 -0500

    delete item on longclick

--- MainActivity.java
+++ MainActivity.java
@@ -3,6 +3,7 @@ package com.example.tenorio.todoapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -24,6 +25,14 @@ public class MainActivity extends AppCompatActivity {
         lvItems.setAdapter(itemsAdapter);
         items.add("First Item");
         items.add("Second Item");
+        lvItems.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                items.remove(position);
+                itemsAdapter.notifyDataSetChanged();
+                return true;
+            }
+        });
     }
 
     public void onAddItem(View v) {
@@ -32,4 +41,5 @@ public class MainActivity extends AppCompatActivity {
         itemsAdapter.add(itemText);
         etNewItem.setText("");
     }
+
 }
